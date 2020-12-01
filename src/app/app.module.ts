81a963c4a218f766ba8de4109df66681171adf72
MeesterMarcus <lorenzanamarcus@gmail.com>
2020-12-01 15:33:44 -0600

    adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NameDrawComponent } from './name-draw/name-draw.component';
 import {FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NameDrawComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
